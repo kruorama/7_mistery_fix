6cb9c7cf7d60832618958ca8598d124bca8fcd34
Aleksandra Katonina <aleksandra.katonina@klarna.com>
2018-09-01 13:00:49 +0200

    Solved

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,11 +1,24 @@
 from math import sqrt
+import sys
 
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
+    if discriminant < 0:
+        return None, None
+
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
+
     if discriminant == 0:
         return root1, None
     else:
         return root1, root2
+
+
+if __name__ == '__main__':
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    c = int(sys.argv[3])
+
+    print(get_roots(a, b, c))
